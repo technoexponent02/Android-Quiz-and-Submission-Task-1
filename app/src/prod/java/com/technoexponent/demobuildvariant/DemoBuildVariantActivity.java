912f9879e85a24edde5055e194ae1862234f059d
Technoexponent <technoexponent02@gmail.com>
2019-06-19 19:48:40 +0530

    first commit

---
+++ app/src/prod/java/com/technoexponent/demobuildvariant/DemoBuildVariantActivity.java
@@ -0,0 +1,13 @@
+package com.technoexponent.demobuildvariant;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DemoBuildVariantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_build_variant);
+    }
+}
---
+++ app/src/qa/java/com/technoexponent/demobuildvariant/DemoBuildVariantActivity.java
@@ -0,0 +1,13 @@
+package com.technoexponent.demobuildvariant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DemoBuildVariantActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_demo_build_variant);
+    }
+}
